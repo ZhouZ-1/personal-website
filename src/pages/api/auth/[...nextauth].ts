7b9e87209be5213d77c735c28569a777c29269a7
ZhouZ <16379978+ZhouZ-1@users.noreply.github.com>
2022-08-22 13:40:27 +1000

    Temp fix for build error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,10 +10,10 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     // ...add more providers here
     CredentialsProvider({
       name: "Credentials",
