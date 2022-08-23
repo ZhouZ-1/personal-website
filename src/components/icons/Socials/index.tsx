68959d5eecf7251909407ae364e136c89d93ad30
ZhouZ <16379978+ZhouZ-1@users.noreply.github.com>
2022-08-23 12:18:44 +1000

    Adjust transition to only affect transform

--- src/components/icons/Socials/index.tsx
+++ src/components/icons/Socials/index.tsx
@@ -8,28 +8,28 @@ const Socials = () => {
     <section className="text-white flex md:text-xl mt-3 md:space-x-5 space-x-2">
       <a
         href="https://github.com/ZhouZ-1"
-        className="hover:scale-110 transition-all ease-out flex justify-center items-center space-x-1"
+        className="hover:scale-110 transition-transform ease-out flex justify-center items-center space-x-1"
       >
         <GitHub />
         <span>GitHub</span>
       </a>
       <a
         href="https://www.linkedin.com/in/zhouz1/"
-        className="hover:scale-110 transition-all ease-out flex justify-center items-center space-x-1"
+        className="hover:scale-110 transition-transform ease-out flex justify-center items-center space-x-1"
       >
         <LinkedIn />
         <span>LinkedIn</span>
       </a>
       <a
         href="https://zzhou.dev/resume_public.pdf"
-        className="hover:scale-110 transition-all ease-out flex justify-center items-center space-x-1"
+        className="hover:scale-110 transition-transform ease-out flex justify-center items-center space-x-1"
       >
         <ResumeIcon />
         <span>Resume</span>
       </a>
       <a
         href="mailto:hello@zzhou.dev"
-        className="hover:scale-110 transition-all ease-out flex justify-center items-center space-x-1"
+        className="hover:scale-110 transition-transform ease-out flex justify-center items-center space-x-1"
       >
         <MailIcon
           className="flex-shrink-0 h-6 w-6 text-white"
